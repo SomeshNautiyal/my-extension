3f8cd9df856927d6213d53dd870a788643db6da0
Bradley.Somesh <bradley.somesh@rubicotech.in>
2025-01-08 15:23:45 +0530

    initial setup

--- popup.js
+++ popup.js
@@ -4,6 +4,7 @@ document.getElementById("changeColor").addEventListener("click", () => {
         target: { tabId: tabs[0].id },
         func: () => {
           document.body.style.backgroundColor = "yellow";
+          console.log("Color changed to yellow!");
         },
       });
     });
