ed20f5cc18709c4f7025c3383fb7694869dc8e87
Bradley.Somesh <bradley.somesh@rubicotech.in>
2025-01-08 16:05:26 +0530

    add:feature of setting background colour

--- content.js
+++
@@ -1,2 +0,0 @@
-console.log("Content script loaded successfully!");
-document.body.style.border = "5px solid red";
--- popup.js
+++ popup.js
@@ -1,12 +1,48 @@
-document.getElementById("changeColor").addEventListener("click", () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+document.addEventListener("DOMContentLoaded", () => {
+  const newColorInput = document.getElementById("newColorInput");
+  const resetColorButton = document.getElementById("resetColorButton");
+  const applyColorButton = document.getElementById("applyColorButton");
+  const closeExtensionButton = document.getElementById("closeExtension");
+
+  applyColorButton.addEventListener("click", async () => {
+    const selectedColor = newColorInput.value;
+    if (selectedColor) {
+      // Get the active tab and send a message to the content script
+      const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
       chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
-        func: () => {
-          document.body.style.backgroundColor = "yellow";
-          console.log("Color changed to yellow!");
+        target: { tabId: tab.id },
+        func: (color) => {
+          document.body.style.backgroundColor = color;
         },
+        args: [selectedColor],
       });
-    });
+    }
   });
-  
+
+  resetColorButton.addEventListener("click", async () => {
+    // Get the active tab and send a message to the content script
+    resetColour();
+  });
+
+
+  if(closeExtensionButton){
+      //close extension
+      closeExtensionButton.addEventListener("click", async () => {
+        window.close();
+        resetColour();
+         
+      });
+  }
+
+  async function resetColour() {
+    // Get the active tab and send a message to the content script
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: () => {
+        document.body.style.backgroundColor = "";
+      },
+    });
+  }
+
+});
